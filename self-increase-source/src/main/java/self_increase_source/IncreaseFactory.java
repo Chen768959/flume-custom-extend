04b48eda19a6feef2849b1991e552e200e71a943
Chen768959 <934103231@qq.com>
2021-06-19 19:05:46 +0800

    自增source-主体

---
+++ self-increase-source/src/main/java/self_increase_source/IncreaseFactory.java
@@ -0,0 +1,20 @@
+package self_increase_source;
+
+import org.apache.flume.Event;
+
+import java.util.List;
+
+/**
+ * @author Chen768959
+ * @date 2021/6/19
+ */
+public interface IncreaseFactory {
+  /**
+   * 自增Event方法
+   * @param increaseNum 每次生成的数量
+   * @author Chen768959
+   * @date 2021/6/19 下午 7:04
+   * @return java.util.List<org.apache.flume.Event>
+   */
+  List<Event> increaseEvents(int increaseNum);
+}
--- self-increase-source/src/main/java/self_increase_source/SelfIncreaseSource.java
+++ self-increase-source/src/main/java/self_increase_source/SelfIncreaseSource.java
@@ -1,34 +1,65 @@
 package self_increase_source;
 
 import org.apache.flume.Context;
+import org.apache.flume.Event;
 import org.apache.flume.EventDeliveryException;
 import org.apache.flume.PollableSource;
-import org.apache.flume.conf.BatchSizeSupported;
 import org.apache.flume.conf.Configurable;
 import org.apache.flume.source.AbstractSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * @author Chen768959
  * @date 2021/6/18
  */
-public class SelfIncreaseSource extends AbstractSource implements
-        PollableSource, Configurable, BatchSizeSupported {
+public class SelfIncreaseSource extends AbstractSource implements Configurable, PollableSource  {
+  private static final Logger logger = LoggerFactory.getLogger(SelfIncreaseSource.class);
 
   private int batchSize;
 
+  private int intervalMs;
+
+  private IncreaseFactory increaseFactory;
+
   @Override
   public Status process() throws EventDeliveryException {
-    return null;
+    Status status = null;
+
+    try {
+      List<Event> events = increaseFactory.increaseEvents(batchSize);
+
+      // 获取ChannelProcessor对象，将events列表交给他
+      getChannelProcessor().processEventBatch(events);
+
+      status = Status.READY;
+    } catch (Throwable t) {
+      logger.error("生成异常",t);
+      status = Status.BACKOFF;
+      // re-throw all Errors
+      if (t instanceof Error) {
+        throw (Error)t;
+      }
+    }
+
+    if (intervalMs>0){
+      try {
+        Thread.sleep(intervalMs);
+      } catch (InterruptedException e) {
+        logger.error("间隔异常：",e);
+      }
+    }
+    return status;
   }
 
   @Override
   public void configure(Context context) {
+    batchSize = context.getInteger("batch-size", 100);
 
-  }
-
-  @Override
-  public long getBatchSize() {
-    return batchSize;
+    // 每次处理完后时间
+    intervalMs = context.getInteger("interval-ms", 2000);
   }
 
   @Override
